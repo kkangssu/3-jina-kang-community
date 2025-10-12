ab3048418a3caf4ebeadd9c47a5cad6e83c2fc31
kkangssu <unikang16@gmail.com>
2025-10-12 17:41:19 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/ktb/ktb_community/KtbCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.ktb_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtbCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktb/ktb_community/KtbCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.ktb_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
