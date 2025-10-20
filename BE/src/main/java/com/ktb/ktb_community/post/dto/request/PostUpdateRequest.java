f4f60cf1a67e727fc421cb275c2ff8bea79314da
kkangssu <unikang16@gmail.com>
2025-10-20 19:53:29 +0900

    refactor: 파일 저장 방식 변경

--- BE/src/main/java/com/ktb/ktb_community/post/dto/request/PostUpdateRequest.java
+++ BE/src/main/java/com/ktb/ktb_community/post/dto/request/PostUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.ktb.ktb_community.post.dto.request;
 
+import com.ktb.ktb_community.global.file.dto.FileInfo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -10,5 +11,7 @@ public record PostUpdateRequest (
     @Size(max = 255, message = "제목은 최대 255자입니다")
     String title,
 
-    String content
+    String content,
+
+    List<FileInfo> postImages
 ){}
